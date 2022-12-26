fa2f65206e8da3923ac3a6aeba007a8b05edc370
Mohamed Rabie <mohamed.rabie.hussein@gmail.com>
2022-12-26 14:47:30 +0200

    update my Search and Shelf

--- src/App.js
+++ src/App.js
@@ -1,16 +1,68 @@
 import './App.css';
-import { useState } from 'react';
-import Shelf from './component/Shelf'
+import React, {useState,useEffect} from 'react';
 
-function App() {
+import * as BooksAPI from "./BooksAPI";
+
+import Shelf from './component/Shelf'
+import Book from "./component/Book";
+
+const App = () => {
 
   const [showSearchPage, setShowSearchpage] = useState(false);
+  
+  // Shelf Books 
+  //==================
+  const [allbooks, setBooks] = useState([]);
+
+  useEffect(() => {
+    BooksAPI.getAll().then((booksData) => {
+      setBooks(booksData);
+    });
+  }, []);
 
   const bookshelves = [
     { title: "Currently Reading", shelfName: "currentlyReading" },
     { title: "Want to Read", shelfName: "wantToRead" },
     { title: "Read", shelfName: "read" },
   ];
+  //==================
+
+  // Search Books
+  //==================
+  const [searchText, setSearchText] = useState("");
+  const [searchBooks, setSearchBooks] = useState([]);
+
+  const getSearchTextBooks = () => {
+    if (searchText.length !== 0) {
+      BooksAPI.search(searchText).then((searchBooks) => {
+        if (!searchBooks.error) {
+          BooksAPI.getAll().then((allbooks) => {
+            setSearchBooks(setDefaultShelves(searchBooks, allbooks));
+          });
+        } else {
+          setSearchBooks([]);
+        }
+      });
+    } else if (searchText.length === 0) {
+      setSearchBooks([]);
+    }
+  };
+
+  const setDefaultShelves = (searchedBooksLocal, allbooks) => {
+    return searchedBooksLocal.map((book) => {
+      for (let i = 0; i < allbooks.length; i++) {
+        if (allbooks[i].id === book.id) {
+          return { ...book, shelf: allbooks[i].shelf };
+        }
+      }
+      return { ...book, shelf: "none" };
+    });
+  };
+
+  useEffect(() => {
+    getSearchTextBooks();
+  }, [searchText]);
+//==================
 
   return (
     <div className="app">
@@ -18,7 +70,7 @@ function App() {
         <div className="search-books">
           <div className="search-books-bar">
             <a className="close-search"
-              onClick={() => setShowSearchpage(showSearchPage)}
+              onClick={() => setShowSearchpage(!showSearchPage)}
             >
               Close
             </a>
@@ -26,11 +78,25 @@ function App() {
               <input
                 type="text"
                 placeholder="Search by title, author, or ISBN"
+                onChange={(event) => setSearchText(event.target.value)}
               />
             </div>
           </div>
           <div className="search-books-results">
-            <ol className="books-grid"></ol>
+          <ol className="books-grid">
+          {searchBooks &&
+            searchBooks.map((book, index) => (
+              <Book
+                key={index}
+                title={book.title}
+                authors={book.authors}
+                imageUrl={book.imageLinks && book.imageLinks.thumbnail}
+                bookshelf={book.shelf}
+                book={book}
+                isSearching
+              />
+            ))}
+        </ol>
           </div>
         </div>
       ) : (
@@ -40,12 +106,19 @@ function App() {
           </div>
           <div className="list-books-content">
             <div>
-              {bookshelves.map((shelf, index) => (
-                <Shelf
-                  key={index}
-                  title={shelf.title}
-                />
-              ))}
+             {bookshelves.map((bookShelf, index) => (
+              <Shelf
+                key={index}
+                title={bookShelf.title}
+                books={
+                  allbooks &&
+                  allbooks.filter(
+                    (allbooks) => allbooks && allbooks.shelf === bookShelf.shelfName
+                  )
+                }
+                setBooks={setBooks}
+              />
+            ))}
             </div>
           </div>
           <div className="open-search">
--- src/component/Book.js
+++ src/component/Book.js
@@ -1,60 +1,63 @@
 import React from "react";
+
 import * as BooksAPI from "../BooksAPI";
 
-const Book = (title, authors, imageUrl, book, setBooks, isSearch, shelf) => {
+const Book = ({ title, authors, imageUrl, book, setBooks, isSearching, bookshelf }) => {
 
-    const getShelfBooks = (event) => {
-        if (event.target.value !== "move") {
-            BooksAPI.update(book, event.target.value).then((response) =>
-                BooksAPI.getAll().then((newBooks) => {
-                    setBooks(newBooks);
-                })
-            );
-        }
-    };
-    const getShelfBooksInSearch = (event) => {
-        if (event.target.value !== "move") {
-            BooksAPI.update(book, event.target.value);
-        }
-    };
-    
-    return (
-        <div className="book">
-            <div className="book-top">
-                <div
-                    className="book-cover"
-                    style={{
-                        width: 128,
-                        height: 193,
-                        backgroundImage: `url("${imageUrl}")`,
-                    }}
-                ></div>
-                <div className="book-shelf-changer">
-                    <select
-                        onChange={(event) => {
-                            if (!isSearch) {
-                                getShelfBooks(event);
-                            } else {
-                                getShelfBooksInSearch(event);
-                            }
-                        }}
-                        defaultValue={shelf}
-                    >
-                        <option value="move" disabled>
-                            Move to...
-                        </option>
-                        <option value="currentlyReading">Current Reading</option>
-                        <option value="wantToRead">Want to Read</option>
-                        <option value="read">Read</option>
-                        <option value="none">None</option>
-                    </select>
-                </div>
-            </div>
-            <div className="book-title">{title}</div>
-            <div className="book-authors">
-                {authors && authors.map((author) => `${author},`)}
-            </div>
+  const getShelfBooks = (event) => {
+    if (event.target.value !== "move") {
+      BooksAPI.update(book, event.target.value).then((response) =>
+        BooksAPI.getAll().then((newBooks) => {
+          setBooks(newBooks);
+        })
+      );
+    }
+  };
+
+  const getShelfBooksInSearch = (event) => {
+    if (event.target.value !== "move") {
+      BooksAPI.update(book, event.target.value);
+    }
+  };
+
+  return (
+    <div className="book">
+      <div className="book-top">
+        <div
+          className="book-cover"
+          style={{
+            width: 128,
+            height: 193,
+            backgroundImage: `url("${imageUrl}")`,
+          }}
+        ></div>
+        <div className="book-shelf-changer">
+          <select
+            onChange={(event) => {
+              if (!isSearching) {
+                getShelfBooks(event);
+              } else {
+                getShelfBooksInSearch(event);
+              }
+            }}
+            defaultValue={bookshelf}
+          >
+            <option value="move" disabled>
+              Move to...
+            </option>
+            <option value="currentlyReading">Current Reading</option>
+            <option value="wantToRead">Want to Read</option>
+            <option value="read">Read</option>
+            <option value="none">None</option>
+          </select>
         </div>
-    );
+      </div>
+      <div className="book-title">{title}</div>
+      <div className="book-authors">
+        {authors && authors.map((author) => `${author},`)}
+      </div>
+    </div>
+  );
 };
+
 export default Book;
